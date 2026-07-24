c71db6662b23664b2878205e7910fcdf97f2e5f1
rynnejatwit <rynnej@wit.edu>
2026-07-24 12:22:08 -0400

    Completed Part A

--- parta.c
+++ parta.c
@@ -9,7 +9,34 @@ int main(int argc, char* argv[]) {
     // Declare the buffer to use. Do NOT change!
     enum { BUFSIZE = 8 };
     char buffer[BUFSIZE];
-    // TODO: Complete and document
-
+ 
+    if (argc < 2) {
+        printf("USAGE: parta FILEIN\n");
+        return 1;
+    }
+ 
+    int fd = open(argv[1], O_RDONLY);
+    if (fd < 0) {
+        printf("ERROR: %s not found\n", argv[1]);
+        return 2;
+    }
+ 
+    const char* names[] = {"Upper", "Lower", "Number", "Space", "Other"};
+    int count[5] = {0};
+ 
+    ssize_t n;
+    while ((n = read(fd, buffer, BUFSIZE)) > 0) {
+        for (ssize_t i = 0; i < n; i++) {
+            unsigned char c = (unsigned char)buffer[i];
+            if (isupper(c))      count[0]++;
+            else if (islower(c)) count[1]++;
+            else if (isdigit(c)) count[2]++;
+            else if (isspace(c)) count[3]++;
+            else                 count[4]++;
+        }
+    }
+    close(fd);
+ 
+    for (int i = 0; i < 5; i++) printf("%s,%d\n", names[i], count[i]);
     return 0;
 }
